6b123751602985321358b1df5e3a3c40ebd59870
iremayvaz <iremayvazz28@gmail.com>
2024-09-29 17:59:14 +0300

    Bathrooms'a göre ortalama fiyat grafiği

--- evFiyatlariAnalizi.py
+++ evFiyatlariAnalizi.py
@@ -2,6 +2,7 @@
 import pandas as pd 
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 # CSV dosyasını yükledim
 housing = pd.read_csv('Housing.csv')
@@ -30,7 +31,7 @@ def status(price):
 
 # Alan - pahalılık durumu
 housing['status'] = housing['price'].apply(status) # tabloya eklendi
-
+'''
 plt.title('Relationship between Area - Expensiveness') # Grafik başlığı
 plt.scatter(housing['area'], housing['status'], s=50, c='red', alpha=0.1, edgecolors='black') # Grafik iskeleti
 
@@ -98,3 +99,45 @@ plt.grid(color='brown', linestyle='--', linewidth=0.5)
 
 # Grafiği göster.
 plt.show()
+'''
+
+# Bathrooms'a göre ortalama
+avgByBathrooms = housing.groupby('bathrooms')['price'].mean()
+
+def avgPrice(numbers_bathrooms):
+    return avgByBathrooms.get(numbers_bathrooms, None)
+        
+housing['avgPrice_toBaths'] = housing['bathrooms'].apply(avgPrice)
+
+plt.title("Average Price Graph For Bathrooms") # Grafik başlığı
+
+colors = ['pink', 'violet', 'yellow', 'orange']
+                                                        
+plt.barh(avgByBathrooms.index, avgByBathrooms.values, color = colors)# Grafik iskeleti
+
+# X Ekseni
+plt.xlabel('Ortalama')
+plt.xticks([1750000, 2900000, 4100000, 5200000, 6400000, 7500000, 8700000, 9850000, 11000000, 13300000])
+
+# Y Ekseni
+plt.ylabel('Bathrooms')
+plt.yticks([1, 2, 3, 4])
+
+# Grafiğin okunurluğunu kolaylaştır
+plt.grid(color='red', linestyle='--', linewidth=0.3)
+
+# Grafiği göster
+plt.show() 
+
+print(avgByBathrooms)
+
+
+'''
+plt.title("Average Price Graph For Bathrooms")
+plt.barh(avgByBathrooms.index, avgByBathrooms.values, color=colors)  # Banyo sayısına göre ortalama fiyat
+plt.xlabel('Average Price')
+plt.ylabel('Bathrooms')
+plt.show()
+'''
+# Furnishingstatus'e göre ortalama
+avgByFurnishing = housing.groupby('furnishingstatus')['price'].mean()
